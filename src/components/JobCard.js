53e63f54a68a50daad2bed3cc62ab9ab8faf126a
admin <test>
2020-07-26 19:23:13 +0530

    master

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -17,22 +17,25 @@ function JobCard({ job }) {
             className="job-pre"
             style={{ display: "flex", justifyContent: "space-between" }}
           >
-            <div className="company-logo">
+            <div className="company-logo col-md-2">
               <Image
                 src={job.company_logo}
                 roundedCircle
-                height="50"
                 alt={job.company}
+                style={{
+                  width: "100%",
+                  height: "auto",
+                }}
               />
             </div>
-            <div className="company-title">
+            <div className="company-title col-md-4">
               <Card.Title>{job.title}</Card.Title>
               <p>
                 <Image src="/location.jpg" alt="location" height="20" />
                 {job.location}
               </p>
             </div>
-            <div className="company-type">
+            <div className="company-type col-md-3">
               <Card.Subtitle className="mb-2 text-muted">
                 {job.company}
               </Card.Subtitle>
@@ -40,18 +43,21 @@ function JobCard({ job }) {
                 {job.type}
               </Badge>
             </div>
-            <div className="company-apply">
-              <a variant="dark" href={job.how_to_apply}>
-                Apply Now
-              </a>
-            </div>
-            <div className="company-view">
+            <div className="company-view col-md-3">
               <Button
                 variant="dark"
+                className="mr-2 "
                 onClick={() => setOpen((prevOpen) => !prevOpen)}
               >
                 {!open ? "View Details" : "Hide details"}
               </Button>
+              <a
+                className="btn btn-dark"
+                _target="blank"
+                href={job.how_to_apply}
+              >
+                Apply Now
+              </a>
             </div>
           </div>
 
